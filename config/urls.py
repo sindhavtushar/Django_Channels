1f90519426a60bf4651afd9826d179362b815a87
tushar sindhav <tusharsindhav09@gmail.com>
2026-02-26 16:46:22 +0530

    feat(chat): integrate chat URLs and base views

--- config/urls.py
+++ config/urls.py
@@ -1,25 +1,13 @@
-"""
-URL configuration for config project.
+# config.urls
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import home
 from apps.livecalculator.views import live_calculator_view
-from apps.echo.views import room
+from apps.echo.views import echo_room
 
 urlpatterns = [
+    path('', home, name='home'), 
     # Django Admin URLs
     path('admin/', admin.site.urls),
     
@@ -27,5 +15,9 @@ urlpatterns = [
     path('calculator/', live_calculator_view, name='live_calc_page'),
     
     # Bingo Game URLs
-    path("bingo/<str:room_name>/", room, name="bingo_room"),
+    path("bingo/<str:room_name>/", echo_room, name="bingo_room"),
+
+    # Chat URLs
+    path('chat/', include('apps.chat.urls')),
+
 ]
